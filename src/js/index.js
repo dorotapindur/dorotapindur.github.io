7cfb35ef1a0980112a13d23578c1f6d688b44f46
Dorota Pindur <dpindur@wp.pl>
2020-11-11 13:15:49 +0100

    fix: adding repo to list if description; const homepage

--- src/js/index.js
+++ src/js/index.js
@@ -19,8 +19,8 @@ fetch('https://api.github.com/users/dorotapindur/repos?sort=created&direction=as
 .then(resp => resp.json())
 .then(resp => {
     for (let repo of resp) {
-        const{name, html_url, description} = repo;
-        console.log(`NAME: ${name} ADDRESS: ${html_url} DESCRIPTION: ${description} `);
+        const{name, html_url, description, homepage} = repo;
+        console.log(`NAME: ${name} ADDRESS: ${html_url} DESCRIPTION: ${description} DEMO: ${homepage}`);
         const cardTemplate = `<li class="card">
         <svg class="card__grey-dots" width="40" height="8" viewBox="0 0 40 8" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="4" cy="4" r="4" fill="#464A4D"/>
@@ -34,11 +34,14 @@ fetch('https://api.github.com/users/dorotapindur/repos?sort=created&direction=as
             <p class="card__category">project:</p> <p class="card__content card__content--project">${name}</p>
             <p class="card__category">description:</p> <p class="card__content card__content--description">${description}</p>
           
-            <p class="card__category card__category--demo">demo:</p> <p class="card__content card__content--demo">&lt;<a class="card__link" href="https://dorotapindur.github.io/${name}/" rel="noopener" target=_blank title="${name} - demo">link to demo</a>&gt;</p>
+            <p class="card__category card__category--demo">demo:</p> <p class="card__content card__content--demo">&lt;<a class="card__link" href="${homepage}" rel="noopener" target=_blank title="${name} - demo">link to demo</a>&gt;</p>
             <p class="card__category">github:</p> <p class="card__content card__content--source">&lt;<a class="card__link" href="${html_url}" rel="noopener" target=_blank title="${name} - repository">source code</a>&gt;</p>
         </div>
       </li>`;
+
+      if (description){
         repositoryList.innerHTML += cardTemplate;
+      }
     }
 })
 .catch(error => {
